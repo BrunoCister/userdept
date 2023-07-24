20d95daed08552e460a4e06f35727e90f1fb498f
Bruno Cister Alves <brunocister@gmail.com>
2023-07-24 16:08:21 -0300

    projeto criado, entidades criadas, banco H2 configurado

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_department(name) VALUES ('Gestao');
+INSERT INTO tb_department(name) VALUES ('Financas');
+INSERT INTO tb_department(name) VALUES ('Recursos Humanos');
+INSERT INTO tb_department(name) VALUES ('Informatica');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria', 'maria@email.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Thiago', 'thiago@email.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (3, 'Alex', 'alex@email.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (4, 'Flavia', 'flavia@email.com');
