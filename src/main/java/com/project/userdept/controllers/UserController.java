d1c48d2cc8ea054ef7395ffddd64aff8afb0e2a3
Bruno Cister Alves <brunocister@gmail.com>
2023-07-27 16:26:17 -0300

    adicao dos outros metodos http

--- src/main/java/com/project/userdept/controllers/UserController.java
+++ src/main/java/com/project/userdept/controllers/UserController.java
@@ -3,9 +3,13 @@ package com.project.userdept.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,20 +25,48 @@ public class UserController {
     private UserRepository userRepository;
     
     @GetMapping
-    public List<User> findAll() {
+    public ResponseEntity findAll() {
         List<User> result = userRepository.findAll();
-        return result;
+        return ResponseEntity.ok().body(result);
     }
 
     @GetMapping(value = "/{id}")
-    public User findById(@PathVariable Long id) {
+    public ResponseEntity findById(@PathVariable Long id) {
         User result = userRepository.findById(id).get();
-        return result;
+        return ResponseEntity.ok().body(result);
     }
 
     @PostMapping
-    public User insert(@RequestBody User user) {
+    public ResponseEntity insert(@RequestBody User user) {
         User result = userRepository.save(user);
-        return result;
+        if (result.getId() != 0) {
+            return ResponseEntity.ok().body(result);
+        }
+        return ResponseEntity.status(500).body("Erro interno.");
+        
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity update(@PathVariable Long id, @RequestBody User user) {
+        user.setId(id);
+        User result = userRepository.save(user);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity patch(@PathVariable Long id, @RequestBody User user) {
+        user.setId(id);
+        User result = userRepository.save(user);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        User result = userRepository.findById(id).get();
+        if (result.getId() == id) {
+            userRepository.deleteById(id);
+            return ResponseEntity.ok().body("Usuario excluido");
+        }
+        return ResponseEntity.status(500).body("Erro interno.");
     }
 }
